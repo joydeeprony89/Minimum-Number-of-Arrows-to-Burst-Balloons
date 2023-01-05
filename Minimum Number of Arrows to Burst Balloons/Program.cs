89b853e1318b7a8fe2ac951f56c3c098fd1f24ca
jd_786 <jaydeep_shil@certree.com>
2023-01-05 13:02:02 +0530

    Add project files.

---
+++ Minimum Number of Arrows to Burst Balloons/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+var points = new int[2][] { new int[] { -2147483646, -2147483645 }, new int[] { 2147483646, 2147483647 } };
+Solution s = new Solution();
+var asnwer = s.FindMinArrowShots(points); 
+Console.WriteLine(asnwer);
+
+
+public class Solution
+{
+  public int FindMinArrowShots(int[][] points)
+  {
+    // O(nlogn)
+    Array.Sort(points, new Comparer());
+    int count = 1;
+    var previous = points[0];
+    for (int i = 1; i < points.Length; i++)
+    {
+      var current = points[i];
+      var cStart = current[0];
+      var cEnd = current[1];
+      var prevStart = previous[0];
+      var prevEnd = previous[1];
+      if (cStart <= prevEnd)
+      {
+        //previous[0] = Math.Min(cStart, prevStart);
+        //previous[1] = Math.Min(cEnd, prevEnd);
+      }
+      else
+      {
+        count++;
+        previous = current;
+      }
+    }
+
+    return count;
+  }
+  public class Comparer : IComparer<int[]>
+  {
+    public int Compare(int[] x, int[] y)
+    {
+      return x[0].CompareTo(y[0]);
+    }
+  }
+}
